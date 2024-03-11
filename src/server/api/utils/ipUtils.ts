e721ff022dc4b4f90379258bd23864c6a0162b68
screendisease <screendisease@gmail.com>
2024-03-12 00:14:33 +0100

    Update ipUtils.ts

--- src/server/api/utils/ipUtils.ts
+++ src/server/api/utils/ipUtils.ts
@@ -126,5 +126,4 @@ export function isValidIP(ip: string): boolean {
 export function isValidDomain(domain: string): boolean {
 	const domainRegex = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/;
 	return true;
-	/*domainRegex.test(domain);*/
 }
